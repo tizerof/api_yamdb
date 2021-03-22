29c525e9ddc10e1b81edddd050be7dbfbc815574
Andrei Rotar <andrei.rotari.work@gmail.com>
2021-03-22 21:08:19 +0300

    Started implementing models: Category, Title

--- api/models.py
+++ api/models.py
@@ -10,3 +10,20 @@ class Reviews(models.Model):
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name='reviews')
     pub_date = models.DateField(auto_now_add=True)
+
+
+class Category(models.Model):
+    """Модель категорий."""
+    name = models.CharField(max_length=30)
+    
+
+class Title(models.Model):
+    """Модель произведений."""
+    name = models.CharField(max_length=50)
+    author = models.CharField(max_length=50)
+    category = models.ForeignKey(
+        'Category',
+        related_name='title',
+        on_delete=models.SET_NULL,
+    )
+    genre = models.ManyToManyField(Genre)

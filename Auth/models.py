9a84a68ed5b9cc72eddb76a2eeef30ddd9c71194
Georgy Satkov <satkov@MacBook-Air-Georgy.local>
2021-03-22 17:48:23 +0300

    users model

---
+++ Auth/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+
+class Users(models.Model):
+    CHOICES = (
+        ('user', 'user'),
+        ('moderator', 'moderator'),
+        ('admin', 'admin'),
+    )
+    username = models.CharField(max_length=100, blank=False, null=False)
+    email = models.EmailField(max_length=254, blank=False, null=False)
+    role = models.CharField(max_length=100, choices=CHOICES, default='user')
+    description = models.TextField(max_length=3000, blank=True, null=True)
+    first_name = models.CharField(max_length=100, blank=True, null=True)
+    last_name = models.CharField(max_length=100, blank=True, null=True)

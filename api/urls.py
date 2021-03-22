eb191e0e4f6aa6970297d43e5748558eb25302a1
Евгений Кочкин <tizer@inbox.ru>
2021-03-22 22:22:16 +0300

    Feature/comments (#6)
    add new model 'Comment' with interface

--- api/urls.py
+++ api/urls.py
@@ -1,13 +1,17 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from .views import ReviewViewSet
+from .views import CommentViewSet, ReviewViewSet
 
 router = DefaultRouter()
 
 
-router.register(r'^titles/(?P<title_id>\d+)/reviews',
-                ReviewViewSet, basename='review')
+router.register(
+    r'^titles/(?P<title_id>\d+)/reviews', ReviewViewSet, basename='review')
+router.register(
+    r'^titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)/comments',
+    CommentViewSet, basename='comment')
+
 
 urlpatterns = [
     path('v1/', include(router.urls)),

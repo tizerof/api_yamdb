db1d98781d58cd3d8498c1764eae40714c035657
Евгений <tizerofspar@yandex.ru>
2021-03-22 23:18:34 +0300

    change score field

--- api/views.py
+++ api/views.py
@@ -1,6 +1,5 @@
 from .serializers import CommentSerializer, ReviewSerializer
 from api.models import Review, Title
-from api.models import Review, Title, User
 from rest_framework import viewsets
 from rest_framework.generics import get_object_or_404
 

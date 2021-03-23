8cb364949c30997488a8d4128b4817c5da5c9d51
Andrei Rotar <andrei.rotari.work@gmail.com>
2021-03-23 20:34:01 +0300

    Added models for Titles, Categories and Genres

--- api/models.py
+++ api/models.py
@@ -4,26 +4,43 @@ from django.db import models
 User = get_user_model()
 
 
-class Reviews(models.Model):
-    """Модель отзывов. """
-    text = models.TextField()
-    author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name='reviews')
-    pub_date = models.DateField(auto_now_add=True)
-
-
 class Category(models.Model):
     """Модель категорий."""
     name = models.CharField(max_length=30)
-    
+    slug = models.SlugField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
+
+class Genre(models.Model):
+    """Модель жанров."""
+    name = models.CharField(max_length=15)
+
+    def __str__(self):
+        return self.name
+
 
 class Title(models.Model):
     """Модель произведений."""
     name = models.CharField(max_length=50)
     author = models.CharField(max_length=50)
+    description = models.TextField(max_length=200)
     category = models.ForeignKey(
-        'Category',
-        related_name='title',
-        on_delete=models.SET_NULL,
+        Category,
+        blank=True,
+        null=True,
+        on_delete=models.SET_NULL
     )
     genre = models.ManyToManyField(Genre)
+    year = models.DateField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Reviews(models.Model):
+    """Модель отзывов. """
+    text = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    pub_date = models.DateField(auto_now_add=True)

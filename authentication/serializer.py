21164cd58a86b608cef13cb409803acaa48c8743
Georgy Satkov <satkov@MacBook-Air-Georgy.local>
2021-03-24 13:36:54 +0300

    re.sub for usernamefield overwriting

--- authentication/serializer.py
+++ authentication/serializer.py
@@ -18,6 +18,7 @@ class UserSerializer(serializers.ModelSerializer):
         model = User
 
     password = serializers.CharField(default='default')
+    username = serializers.CharField(default='default')
 
     def is_valid(self, raise_exception=False):
         email = self.context['request'].POST.get('email')

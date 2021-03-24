b67a065d896cf656c92180b6927e75662ac12f16
Georgy Satkov <satkov@MacBook-Air-Georgy.local>
2021-03-24 13:24:55 +0300

    add SpecificUserViewSet, change model

--- api_yamdb/settings.py
+++ api_yamdb/settings.py
@@ -27,6 +27,7 @@ INSTALLED_APPS = [
     'authentication',
     'rest_framework',
     'api',
+    'django_filters',
 ]
 
 MIDDLEWARE = [
@@ -78,7 +79,9 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 100
+    'PAGE_SIZE': 100,
+    'DEFAULT_FILTER_BACKENDS':
+        ['django_filters.rest_framework.DjangoFilterBackend'],
 }
 
 SIMPLE_JWT = {

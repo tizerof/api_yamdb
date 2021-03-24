a212af1c2c215c584f8c95feffdb1c0ef11be5ee
Евгений <tizerofspar@yandex.ru>
2021-03-24 14:51:38 +0300

    Add permissions to admin and moderator

--- api/models.py
+++ api/models.py
@@ -1,8 +1,7 @@
-from django.contrib.auth import get_user_model
 from django.db import models
 from django.db.models.deletion import CASCADE
 
-User = get_user_model()
+from authentication.models import User
 
 
 class Title(models.Model):

acf1a7b540419f7e913def42df1cf644d275e046
Andrei Rotar <andrei.rotari.work@gmail.com>
2021-03-24 18:46:59 +0300

    can now import genre, title, category objects from csv

--- csvfile/views.py
+++ csvfile/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
-
-import csv, io
+from datetime import datetime
+import csv, io, os
 from django.shortcuts import render
 from django.contrib import messages
 
-from api.models import Category
+from api.models import Category, Genre, Title
 
 def category_upload(request):
     template = 'category_upload.html'
@@ -22,26 +22,29 @@ def category_upload(request):
     io_string = io.StringIO(data_set)
     next(io_string)
     
-    for column in csv.reader(io_string, delimiter=',', quotechar='|'):
+    for column in csv.reader(io_string, delimiter=',', quotechar='"'):
         if csv_file.name == 'category.csv':
             _, created = Category.objects.update_or_create(
                 id=column[0],
                 name=column[1],
                 slug=column[2]
             )
-        elif csv_file.name == 'comments':
-            _, created = Category.objects.update_or_create(
-                id=column[0],
-                review_id=column[1],
-                text=column[2],
-                author=column[3],
-                pub_date=column[4]
-            )
         elif csv_file.name == 'genre.csv':
-            _, created = Category.objects.update_or_create(
+            _, created = Genre.objects.update_or_create(
                 id=column[0],
                 name=column[1],
-                slug=column[2],
+                slug=column[2]
+            )
+        elif csv_file.name == 'titles.csv':
+            # os.system('clear')
+            # print(Category.objects.get(id=int(column[3])))
+            # print(column[3])
+            # input()
+            _, created = Title.objects.update_or_create(
+                id=column[0],
+                name=column[1],
+                year=datetime.strptime(column[2], '%Y'),
+                category=Category.objects.get(id=int(column[3]))
             )
     context= {}
     return render(request, template, context)

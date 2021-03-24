3703d2b54ae17d1e89582f3f5afe93024009f584
Евгений <tizerofspar@yandex.ru>
2021-03-24 20:51:44 +0300

    change urls

--- api/urls.py
+++ api/urls.py
@@ -3,8 +3,8 @@ from rest_framework.routers import DefaultRouter
 
 from .views import CommentViewSet, ReviewViewSet
 
-router = DefaultRouter()
 
+router = DefaultRouter()
 
 router.register(
     r'^titles/(?P<title_id>\d+)/reviews', ReviewViewSet, basename='review')
@@ -14,5 +14,6 @@ router.register(
 
 
 urlpatterns = [
+    path('v1/', include('authentication.urls')),
     path('v1/', include(router.urls)),
 ]

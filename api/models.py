ced57df22ca7535d47dee1476727517cb116a71f
Andrei Rotar <andrei.rotari.work@gmail.com>
2021-03-28 22:02:09 +0300

    Tweaked genre, author, description fields on Title

--- api/models.py
+++ api/models.py
@@ -33,8 +33,9 @@ class Title(models.Model):
         default=None,
         on_delete=models.SET_NULL
     )
-    author = models.CharField(max_length=50)
-    description = models.TextField(max_length=200)
+    genre = models.ManyToManyField(Genre)
+    author = models.CharField(max_length=50, blank=True, null=True)
+    description = models.TextField(max_length=200, blank=True, null=True)
 
     def __str__(self):
         return self.name
